806de44ebc7d78452d10921bb4fde02be98fe888
George  Pianka <gpianka1@gmail.com>
2019-05-04 19:39:28 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw 
 # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-post 'songs/upload', to: 'songs#upload'
+  post 'songs/upload', to: 'songs#upload'
 
   
 end
